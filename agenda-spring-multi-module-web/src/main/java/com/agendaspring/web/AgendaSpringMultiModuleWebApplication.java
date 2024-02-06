96131a969c46c6e241d7bdf028e28c6bbccaa581
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-06 16:14:29 -0300

    Criação dos projetos.

---
+++ agenda-spring-multi-module-web/src/main/java/com/agendaspring/web/AgendaSpringMultiModuleWebApplication.java
@@ -0,0 +1,13 @@
+package com.agendaspring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaSpringMultiModuleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaSpringMultiModuleWebApplication.class, args);
+	}
+
+}
