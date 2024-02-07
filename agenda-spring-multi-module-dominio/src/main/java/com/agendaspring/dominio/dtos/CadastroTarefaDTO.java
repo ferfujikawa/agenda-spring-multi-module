45d21df941f0722e44a1ac56b2e0c1b1f850a1f5
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-07 16:03:14 -0300

    Criação do DTO para cadastro de Tarefa.

---
+++ agenda-spring-multi-module-dominio/src/main/java/com/agendaspring/dominio/dtos/CadastroTarefaDTO.java
@@ -0,0 +1,52 @@
+package com.agendaspring.dominio.dtos;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class CadastroTarefaDTO {
+			
+	@NotBlank(message = "É necessário informar um título para a tarefa")
+	private String titulo;
+	
+	@NotNull(message = "É necessário informar um prazo para a tarefa")
+	@FutureOrPresent(message = "O prazo da tarefa não pode ser retroativo")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	private LocalDateTime prazo;
+	
+	private String anotacao;
+
+	public CadastroTarefaDTO(String titulo, LocalDateTime prazo, String anotacao) {
+		this.titulo = titulo;
+		this.prazo = prazo;
+		this.anotacao = anotacao;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public LocalDateTime getPrazo() {
+		return prazo;
+	}
+
+	public String getAnotacao() {
+		return anotacao;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setPrazo(LocalDateTime prazo) {
+		this.prazo = prazo;
+	}
+
+	public void setAnotacao(String anotacao) {
+		this.anotacao = anotacao;
+	}
+}
