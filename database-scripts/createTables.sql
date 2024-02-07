80c10abc88ce2b2e13a5af5fc5fc60aba84cdb11
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-07 14:45:33 -0300

    Script de criação das tabelas.

---
+++ database-scripts/createTables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tarefas (
+	id uuid NOT NULL,
+	data_cadastro timestamp(6) NULL,
+	prazo timestamp(6) NULL,
+	situacao varchar(255) NULL,
+	titulo varchar(255) NULL,
+	visualizada bool NULL,
+	valor varchar(255) NULL,
+	CONSTRAINT tarefas_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE historicos_tarefa (
+	id uuid NOT NULL,
+	anotacao varchar(255) NULL,
+	data_historico timestamp(6) NULL,
+	evento varchar(255) NULL,
+	tarefa_id uuid NULL,
+	CONSTRAINT historicos_tarefa_pkey PRIMARY KEY (id),
+	CONSTRAINT historicos_tarefa_tarefa_fk FOREIGN KEY (tarefa_id) REFERENCES tarefas(id)
+);
