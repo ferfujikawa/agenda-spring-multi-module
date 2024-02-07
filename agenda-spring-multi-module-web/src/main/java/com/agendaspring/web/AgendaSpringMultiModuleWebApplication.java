3cd3a8949c2d80c2c1879ce75ea0cd9adc8e9e7e
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-07 14:55:34 -0300

    Correção de escaneamento de pacotes para injeção de dependências.

--- agenda-spring-multi-module-web/src/main/java/com/agendaspring/web/AgendaSpringMultiModuleWebApplication.java
+++ agenda-spring-multi-module-web/src/main/java/com/agendaspring/web/AgendaSpringMultiModuleWebApplication.java
@@ -2,8 +2,12 @@ package com.agendaspring.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.agendaspring")
+@EntityScan(basePackages = "com.agendaspring.dominio.entities")
+@EnableJpaRepositories("com.agendaspring.infra.repositories")
 public class AgendaSpringMultiModuleWebApplication {
 
 	public static void main(String[] args) {
