191d45e48d6761ce9237d3b58ab93da50027735f
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-08 13:28:35 -0300

    Criação de DTO para ação de registrar anotação em tarefa.

---
+++ agenda-spring-multi-module-dominio/src/main/java/com/agendaspring/dominio/dtos/RegistrarAnotacaoTarefaDTO.java
@@ -0,0 +1,14 @@
+package com.agendaspring.dominio.dtos;
+
+public class RegistrarAnotacaoTarefaDTO {
+
+	private String anotacao;
+
+	public String getAnotacao() {
+		return anotacao;
+	}
+
+	public void setAnotacao(String anotacao) {
+		this.anotacao = anotacao;
+	}
+}
