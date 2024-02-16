bf704de8e0736d1cb2e8f93fd629b0ec454df320
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-16 16:27:48 -0300

    Criação da classe de configuração de security no módulo api.

---
+++ agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/configs/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.agendaspring.api.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(config ->
+            config.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
