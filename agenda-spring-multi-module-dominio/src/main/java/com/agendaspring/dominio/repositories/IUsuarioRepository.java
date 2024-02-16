feac9f3f05c79657ca669cb7e0ca014554d39328
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-16 16:28:33 -0300

    Adição do método findByLogin em UsuarioRepository.

--- agenda-spring-multi-module-dominio/src/main/java/com/agendaspring/dominio/repositories/IUsuarioRepository.java
+++ agenda-spring-multi-module-dominio/src/main/java/com/agendaspring/dominio/repositories/IUsuarioRepository.java
@@ -1,5 +1,9 @@
 package com.agendaspring.dominio.repositories;
 
+import com.agendaspring.dominio.entities.Usuario;
+
 public interface IUsuarioRepository {
+
+    Usuario findByLogin(String login);
 
 }
--- agenda-spring-multi-module-infra/src/main/java/com/agendaspring/infra/repositories/UsuarioRepository.java
+++ agenda-spring-multi-module-infra/src/main/java/com/agendaspring/infra/repositories/UsuarioRepository.java
@@ -3,6 +3,7 @@ package com.agendaspring.infra.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.agendaspring.dominio.entities.Usuario;
@@ -11,4 +12,7 @@ import com.agendaspring.dominio.repositories.IUsuarioRepository;
 @Repository
 public interface UsuarioRepository extends IUsuarioRepository, JpaRepository<Usuario, UUID> {
 
+    @Override
+    @Query("SELECT u FROM Usuario u WHERE login = :login")
+    Usuario findByLogin(String login);
 }
