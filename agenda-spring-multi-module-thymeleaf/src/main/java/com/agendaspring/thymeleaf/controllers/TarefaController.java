5c66f3ca8e89b74c1ffff75f2f47d5cf96559c7f
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-16 10:52:02 -0300

    Criação da classe abstrata BaseController com propriedade apiUrl.

--- agenda-spring-multi-module-thymeleaf/src/main/java/com/agendaspring/thymeleaf/controllers/TarefaController.java
+++ agenda-spring-multi-module-thymeleaf/src/main/java/com/agendaspring/thymeleaf/controllers/TarefaController.java
@@ -1,16 +1,19 @@
 package com.agendaspring.thymeleaf.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("tarefas")
-public class TarefaController {
+public class TarefaController extends BaseController {
 
-    @GetMapping
-    public String exibirPaginaListaTarefas() {
-		
-		return "tarefas/index";
-	}
+  @GetMapping
+  public String exibirPaginaListaTarefas(Model model) {
+  
+    model.addAttribute("apiUrl", this.getApiUrl());
+
+    return "tarefas/index";
+  }
 }
