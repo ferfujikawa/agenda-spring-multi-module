5d4132fa88b5de5426c2a82a0f5e2620f8ed4fbd
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-16 16:28:55 -0300

    Criação da interface do serviço de autenticação e sua classe de implementação.

---
+++ agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/services/AutenticacaoService.java
@@ -0,0 +1,27 @@
+package com.agendaspring.api.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.agendaspring.dominio.entities.Usuario;
+import com.agendaspring.dominio.repositories.IUsuarioRepository;
+
+@Service
+public class AutenticacaoService implements IAutenticacaoService {
+
+    private IUsuarioRepository usuarioRepository;
+
+    public AutenticacaoService(IUsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        Usuario usuarioEncontrado = usuarioRepository.findByLogin(username);
+        return new User(usuarioEncontrado.getLogin(), usuarioEncontrado.getSenha(), null);
+    }
+
+}
