18e9c51f430644e44029027e1cf8033849fb9a89
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-19 13:33:37 -0300

    Alteração da configuração de segurança para liberar acesso sem autenticação apenas para a rota /login e solicitar autenticação nas demais rotas. Adição do securityFilter antes da execução do filtro de segurança do Spring Boot.

--- agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/configs/SecurityConfiguration.java
+++ agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/configs/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.agendaspring.api.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -10,23 +11,35 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.agendaspring.api.filters.SecurityFilter;
 import com.agendaspring.api.services.IAutenticacaoService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    private SecurityFilter securityFilter;
+
+    public SecurityConfiguration(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(config -> {
-                config.anyRequest().permitAll();
-            })
-            .build();
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeRequests(requests ->
+                    requests
+                        .antMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // colocando o filtro criado antes do filtro do Spring
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
@@ -39,7 +52,7 @@ public class SecurityConfiguration {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    
     @Bean
     public DaoAuthenticationProvider authenticationProvider(IAutenticacaoService autenticacaoService) {
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
