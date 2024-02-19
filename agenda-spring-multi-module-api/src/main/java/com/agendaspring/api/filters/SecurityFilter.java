b69ee07c38df344ef422dc1cf9ee338a2c355702
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-19 13:32:06 -0300

    Inclusão da autenticação no spring security dentro da classe SecurityFilter.

--- agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/filters/SecurityFilter.java
+++ agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/filters/SecurityFilter.java
@@ -1,35 +1,52 @@
 package com.agendaspring.api.filters;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.agendaspring.api.services.ITokenService;
+import com.agendaspring.dominio.entities.Usuario;
+import com.agendaspring.dominio.repositories.IUsuarioRepository;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     private ITokenService tokenService;
+    private IUsuarioRepository usuarioRepository;
 
-    public SecurityFilter(ITokenService tokenService) {
+    public SecurityFilter(
+        ITokenService tokenService,
+        IUsuarioRepository usuarioRepository) {
+
         this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
+
         String token = recuperarToken(request);
 
-        String subject = tokenService.getSubject(token);
+        if (token != null) {
+            String subject = tokenService.getSubject(token);
 
-        System.out.println(subject);
+            Usuario usuarioLogado = usuarioRepository.findByLogin(subject);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(usuarioLogado, null, Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         filterChain.doFilter(request, response);
     }
@@ -37,11 +54,11 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String recuperarToken(HttpServletRequest request) {
         
         String cabecalho = request.getHeader("Authorization");
-        if (cabecalho == null) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        if (cabecalho != null) {
+            return cabecalho.replace("Bearer ", "");
         }
 
-        return cabecalho.replace("Bearer ", "");
+        return null;
     }
 
 }
