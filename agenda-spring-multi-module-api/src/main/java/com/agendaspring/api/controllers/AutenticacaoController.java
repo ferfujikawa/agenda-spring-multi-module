aa3014c71deeb8f3f45b18e2c15be22e6da63697
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-19 09:48:50 -0300

    Inclusão do retorno do token ao realizar login.

--- agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/controllers/AutenticacaoController.java
+++ agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/controllers/AutenticacaoController.java
@@ -6,21 +6,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.agendaspring.api.services.IAutenticacaoService;
+import com.agendaspring.api.services.ITokenService;
 import com.agendaspring.dominio.dtos.EfetuarLoginDTO;
+import com.agendaspring.dominio.entities.Usuario;
 
 @Controller
 @RequestMapping("login")
 public class AutenticacaoController {
 
     private AuthenticationManager manager;
+    private ITokenService tokenService;
+    private IAutenticacaoService autenticacaoService;
 
-    public AutenticacaoController(AuthenticationManager manager) {
+    public AutenticacaoController(
+        AuthenticationManager manager,
+        ITokenService tokenService,
+        IAutenticacaoService autenticacaoService) {
+
         this.manager = manager;
+        this.tokenService = tokenService;
+        this.autenticacaoService = autenticacaoService;
     }
 
     @PostMapping
@@ -29,6 +41,7 @@ public class AutenticacaoController {
         Authentication token = new UsernamePasswordAuthenticationToken(efetuarLoginDTO.getLogin(), efetuarLoginDTO.getSenha());
         Authentication authenticate = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        Usuario usuarioLogado = autenticacaoService.obterUsuarioPeloLogin(((UserDetails) authenticate.getPrincipal()).getUsername());
+        return ResponseEntity.ok(tokenService.gerarToken(usuarioLogado));
     }
 }
