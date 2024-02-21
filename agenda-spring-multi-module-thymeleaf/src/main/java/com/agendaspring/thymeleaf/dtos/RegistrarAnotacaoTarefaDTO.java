b9425e0a2fe46624e5afb479d82f90f9327d9b53
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-21 15:13:13 -0300

    Cópia de RegistrarAnotacaoTarefaDTO do módulo dominio para thymeleaf.

---
+++ agenda-spring-multi-module-thymeleaf/src/main/java/com/agendaspring/thymeleaf/dtos/RegistrarAnotacaoTarefaDTO.java
@@ -0,0 +1,20 @@
+package com.agendaspring.thymeleaf.dtos;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+public class RegistrarAnotacaoTarefaDTO {
+
+	@NotBlank(message = "Informe uma anotação")
+	@Length(min = 3, message = "A anotação precisa ter pelo menos 3 caracteres")
+	private String anotacao;
+
+	public String getAnotacao() {
+		return anotacao;
+	}
+
+	public void setAnotacao(String anotacao) {
+		this.anotacao = anotacao;
+	}
+}
