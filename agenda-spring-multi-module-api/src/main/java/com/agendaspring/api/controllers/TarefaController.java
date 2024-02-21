b678a5c9c35cde7911bf3d65f1cec4ff490ef172
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-21 14:48:58 -0300

    Correção da validação do registro de anotação.

--- agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/controllers/TarefaController.java
+++ agenda-spring-multi-module-api/src/main/java/com/agendaspring/api/controllers/TarefaController.java
@@ -10,8 +10,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -87,7 +85,7 @@ public class TarefaController {
 	@PostMapping("{id}/registrar-anotacao")
 	public ResponseEntity<String> registrarAnotacao(
 			@PathVariable UUID id,
-			@ModelAttribute("registroAnotacao") RegistrarAnotacaoTarefaDTO registroAnotacao) {
+			@Valid @ModelAttribute("registroAnotacao") RegistrarAnotacaoTarefaDTO registroAnotacao) {
 		
 		if (tarefaService.registrarAnotacao(id, registroAnotacao)) {
 			return ResponseEntity.ok().body("Anotação registrada");
--- agenda-spring-multi-module-dominio/src/main/java/com/agendaspring/dominio/dtos/RegistrarAnotacaoTarefaDTO.java
+++ agenda-spring-multi-module-dominio/src/main/java/com/agendaspring/dominio/dtos/RegistrarAnotacaoTarefaDTO.java
@@ -1,7 +1,13 @@
 package com.agendaspring.dominio.dtos;
 
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
 public class RegistrarAnotacaoTarefaDTO {
 
+	@NotBlank(message = "Informe uma anotação")
+	@Length(min = 3, message = "A anotação precisa ter pelo menos 3 caracteres")
 	private String anotacao;
 
 	public String getAnotacao() {
