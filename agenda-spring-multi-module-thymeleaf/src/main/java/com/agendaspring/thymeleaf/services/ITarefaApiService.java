b09b05e461aea178b6474151b48afe8e305c6f28
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-21 14:09:21 -0300

    Adição do método lsitarHistoricoDeTarefa em TarefaApiService.

--- agenda-spring-multi-module-thymeleaf/src/main/java/com/agendaspring/thymeleaf/services/ITarefaApiService.java
+++ agenda-spring-multi-module-thymeleaf/src/main/java/com/agendaspring/thymeleaf/services/ITarefaApiService.java
@@ -1,10 +1,16 @@
 package com.agendaspring.thymeleaf.services;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 
 import com.agendaspring.thymeleaf.dtos.CadastroTarefaDTO;
+import com.agendaspring.thymeleaf.dtos.HistoricoTarefaDTO;
+import com.agendaspring.thymeleaf.dtos.ListaPaginadaDTO;
 
 public interface ITarefaApiService {
 
     ResponseEntity<?> cadastrarTarefa(CadastroTarefaDTO cadastroTarefaDTO);
+
+    ListaPaginadaDTO<HistoricoTarefaDTO> listarHistoricoDeTarefa(UUID tarefaId, int pagina, int tamanhoPagina);
 }
--- agenda-spring-multi-module-thymeleaf/src/main/java/com/agendaspring/thymeleaf/services/TarefaApiService.java
+++ agenda-spring-multi-module-thymeleaf/src/main/java/com/agendaspring/thymeleaf/services/TarefaApiService.java
@@ -1,12 +1,18 @@
 package com.agendaspring.thymeleaf.services;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.agendaspring.thymeleaf.dtos.CadastroTarefaDTO;
+import com.agendaspring.thymeleaf.dtos.HistoricoTarefaDTO;
+import com.agendaspring.thymeleaf.dtos.ListaPaginadaDTO;
 import com.agendaspring.thymeleaf.dtos.TarefaDTO;
 
 @Service
@@ -30,6 +36,25 @@ public class TarefaApiService implements ITarefaApiService {
         } catch (HttpClientErrorException ex) {
             return ResponseEntity.badRequest().body(ex);
         }
+    }
+
+    @Override
+    public ListaPaginadaDTO<HistoricoTarefaDTO> listarHistoricoDeTarefa(UUID tarefaId, int pagina, int tamanhoPagina) {
+
+        ResponseEntity<ListaPaginadaDTO<HistoricoTarefaDTO>> response = restTemplate.exchange(
+            apiUrl + "/tarefas/{tarefaId}/historico?pagina={pagina}&tamanhoPagina={tamanhoPagina}",
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<ListaPaginadaDTO<HistoricoTarefaDTO>>() {},
+            tarefaId,
+            pagina,
+            tamanhoPagina);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody();
+        }
+
+        return null;
     }
 
 }
